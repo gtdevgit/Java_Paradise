5f4591d1471ed32fe2e052284678f4dae54d667b
Guillaume Toussaint <gtdev.git@gmail.com>
2022-02-11 14:44:09 +0100

    add ConnectionManager

---
+++ src/com/training/paradise/util/ConnectionManager.java
@@ -0,0 +1,41 @@
+package com.training.paradise.util;
+
+import com.training.paradise.configuration.ApplicationConfig;
+import com.training.paradise.configuration.KeyNotFoundException;
+import com.training.paradise.configuration.ValueNotFoundException;
+
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static Connection connection;
+
+    private static Connection getConnection(){
+
+        if (connection == null){
+            ApplicationConfig applicationConfig = new ApplicationConfig();
+            try {
+                applicationConfig.load();
+                Class.forName(applicationConfig.getForName());
+                connection = DriverManager.getConnection(applicationConfig.getUrl(),
+                        applicationConfig.getUser(),
+                        applicationConfig.getPassword());
+            } catch (ClassNotFoundException | SQLException | FileNotFoundException | KeyNotFoundException | ValueNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    private static void Close(){
+        if (connection != null){
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
