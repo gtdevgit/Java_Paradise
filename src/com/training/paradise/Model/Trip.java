e0c2083f59930f0939ab515e2d148fe65ce8beb1
Orickolat <caro.remi.marcel@gmail.com>
2022-02-11 14:55:16 +0100

    ObjectCreation

---
+++ src/com/training/paradise/Model/Trip.java
@@ -0,0 +1,50 @@
+package com.training.paradise.Model;
+
+import java.io.Serializable;
+
+public class Trip implements Serializable {
+
+    private long id;
+    private String departure;
+    private String destination;
+    private Float price;
+
+    public Trip(long id, String departure, String destination, Float price) {
+        this.id = id;
+        this.departure = departure;
+        this.destination = destination;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
